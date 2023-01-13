e3e0d491c4e3f32e716be0b86f73137b7a02cd4c
Robert0101 <rob.tangled@gmail.com>
2023-01-13 19:19:15 -0300

    Agregar Tags al user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-
-import { User } from '@prisma/client';
+import { Body, Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { GetAuthInfo, GetUser } from '../auth/decorator';
 // import { JwtGuard } from '../auth/guard';
 import { Auth0Guard } from '../auth0/auth0.guard';
-import { EditUserProfileDto } from './dto';
 import { ReqAuthDto } from './dto/req-auth.dto';
 import { UserService } from './user.service';
 
 // @UseGuards(JwtGuard)
 @UseGuards(Auth0Guard)
 @Controller('users')
+@ApiBearerAuth('JWT-auth')
+@ApiTags('User')
 export class UserController {
   constructor(private userService: UserService) {}
 
@@ -22,12 +22,24 @@ export class UserController {
   // }
 
   @Get('userinfo')
+  @ApiOperation({
+    summary:
+      'A SignIn or/and SignUp automatic operation that returns the user doc.',
+    description:
+      "Responds with the {...User_document + groupedDocs} that contains the user's Source docs ordered in array depending the 'category' key of each Source. If the user doesn't exists in the Data Base, it will automaticly create it and returns the newly created User document.",
+  })
   async getOrAndCreateUser(@GetAuthInfo() reqAuth: ReqAuthDto) {
     const response = await this.userService.getUserWithGroupedDocs(reqAuth);
     return response;
   }
 
+  // @ApiExcludeEndpoint()
   @Get('exists')
+  @ApiOperation({
+    summary:
+      'Responds with msg: true/false depending if the user exists in the data base',
+  })
+  // @ApiBearerAuth('JWT-auth')
   async userExists(@GetAuthInfo() reqAuth) {
     const userExists = await this.userService.userExists(
       reqAuth.sub,
@@ -35,13 +47,4 @@ export class UserController {
     );
     return userExists;
   }
-
-  //   @Patch('profile')
-  //   editUserProfile(
-  //     @GetUser('id') userId: string,
-  //     @Body() dto: EditUserProfileDto,
-  //   ) {
-  //     return 'edited user';
-  //     // return this.userService.editUser(userId, dto);
-  //   }
 }
